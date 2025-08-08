942f5cea0300eb4601bd3395532c19bf5019b627
jhbradee <jessica.hope.bradee@gmail.com>
2025-08-08 10:14:26 -0700

    Update .gitignore to exclude data directory and revise README for project clarity; add scripts for player lookup and fetching Statcast data

---
+++ src/pybaseball_play_lookup.py
@@ -0,0 +1,4 @@
+# This is a script intended to test the functionality of the pybaseball library and player lookup for Statcast data.
+import pybaseball
+
+pybaseball.playerid_lookup("langford", "wyatt")
